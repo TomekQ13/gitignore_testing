694e3b77ce102ca9ee4e19c4bcbfc552ec772cc6
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-05-01 10:42:45 +0200

    asdasd

--- helper.sh
+++ helper.sh
@@ -2,5 +2,5 @@
 # the first command is triggered on git add - when adding the files to the staging area the lines starting with * are removed
 # the second 
 
-git config filter.deleteInfo.clean "sed '/^*/d'"
-#git config filter.deleteInfo.smudge cat
+git config filter.deleteInfo.clean "sed '/^*/d' && echo 'clean filter was triggerd and successful'"
+git config filter.deleteInfo.smudge "cat && echo 'smudge filter was triggerd and successful'"
