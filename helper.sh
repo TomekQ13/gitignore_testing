c812a4ad8641fa59c68ac92ebb44047fdd9a5251
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-05-01 10:57:31 +0200

    asdads

--- helper.sh
+++ helper.sh
@@ -1,5 +1,4 @@
 # the only thing necessary to do to setup the gitfilter is to run this file
-# the first command is triggered on git add - when adding the files to the staging area the lines starting with * are removed
-# the second 
-
-git config filter.deleteInfo.clean "sed '/^*/d' && echo 'clean filter was triggerd and successful'"
+# the clean .clean attribute of a filter command is triggered on git add - when adding the files to the staging area the lines starting with * are removed
+# more information on these option https://git-scm.com/book/en/v2/Customizing-Git-Git-Attributes
+git config filter.deleteInfo.clean "sed '/^*/d'"
